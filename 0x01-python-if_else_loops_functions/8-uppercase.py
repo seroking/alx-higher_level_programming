68c0ac27ed457cc7bfdc3b2f9b19d6b8c0109963
seroking <baridmohamedamine004@gmail.com>
2023-06-06 16:06:56 -0700

    tasks

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@
 def uppercase(str):
     for word in str:
         if (96 < ord(word) < 123):
-            word = (ord(word) - 32)
-        print("{:c}".format(word), end="")
+            word = chr(ord(word) - 32)
+        print("{}".format(word), end="")
     print("")
