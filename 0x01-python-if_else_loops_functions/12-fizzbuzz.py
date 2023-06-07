dae022bdc8db89b5f10aa1658eddd8b21f05bfb4
seroking <baridmohamedamine004@gmail.com>
2023-06-06 17:31:26 -0700

    tasks

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("fizzbuzz", end="")
+        elif num % 3 == 0:
+            print("Fizz", end="")
+	elif num % 5 == 0:
+            print("buzz", end="")
+        else:
+            print("{}".format(num), end="")
