0c5c2fa28befe10471480642501da6be8c33c2ee
seroking <baridmohamedamine004@gmail.com>
2023-06-08 07:21:49 -0700

    tasks

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+
+    n = len(sys.argv) - 1
+    if n == 0:
+        print("0 arguments.")
+    elif n == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(n))
+    for x in range(n):
+        print("{}: ".format(x + 1, sys.argv[x + 1]))
