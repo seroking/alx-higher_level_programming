67ac3c8f9a26133f12bf9061d837f0a1589f6222
seroking <baridmohamedamine004@gmail.com>
2023-06-08 05:54:40 -0700

    test

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-import add_0 as add
 
-add = add.add
+if __name__ == "__main__":
+    """ print addition """
+
+from add_0 import add
 
 a = 1
 b = 2
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+print("{0} + {1} = {2}".format(a, b, add(a, b)))
