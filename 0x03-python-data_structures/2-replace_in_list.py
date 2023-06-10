3d240d3772135b3ea049c0b17cbd0cbef781f0ab
seroking <baridmohamedamine004@gmail.com>
2023-06-10 15:33:23 -0700

    tasks

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,6 +5,6 @@ def replace_in_list(my_list, idx, element):
         return(my_list)
     else:
         my_list.pop(idx)
-        new_list = my_list.insert(idx, element)
+        my_list.insert(idx, element)
 
-        return(new_list)
+        return(my_list)
