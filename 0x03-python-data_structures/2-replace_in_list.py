4dd966a7eec55693ee2fff4fb49296723b96754d
seroking <baridmohamedamine004@gmail.com>
2023-06-10 15:31:47 -0700

    tasks

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len:
+    if idx < 0 or idx >= len(my_list):
         return(my_list)
     else:
-        my_list.pop([idx])
+        my_list.pop(idx)
         new_list = my_list.insert(idx, element)
 
         return(new_list)
