9bed56bb7965297a11e6750e794e235258a68080
seroking <baridmohamedamine004@gmail.com>
2023-06-10 17:31:27 -0700

    tasks

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for row in matrix:
+            for value in row:
+                print(value, end=" ")
+            print()
