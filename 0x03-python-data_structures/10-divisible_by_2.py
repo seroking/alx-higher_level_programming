5e074c96e2c9dcf9da72828c6ec86be9ba2ab440
seroking <baridmohamedamine004@gmail.com>
2023-06-11 17:56:59 -0700

    taks

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     if my_list:
-        for x in range(len(my_list)):
-            if my_list[x] % 2 == 0:
-                my_list[x] = True
-            else:
-                my_list[x] = False
-
-        return my_list
+        boleen_list = []
+        for slot in my_list:
+            boleen_list.append(slot % 2 == 0)
+        return boleen_list
