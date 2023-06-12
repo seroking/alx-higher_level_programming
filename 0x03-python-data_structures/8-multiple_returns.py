8ed5ada8e681da92ee5131f99cf95fe39de8e086
seroking <baridmohamedamine004@gmail.com>
2023-06-12 15:15:57 -0700

    C again ...

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-    info_tuple = (len(sentence), sentence[0])
-    return(info_tuple)
+    lenght = len(sentence)
+    if lenght:
+        char = sentence[0]
+    else:
+        char = 'None'
+    tuple_info = (lenght, char)
+    return tuple_info
