dd63bf43a5501a3dd42661c1a38dcd41deb665c5
seroking <baridmohamedamine004@gmail.com>
2023-06-14 16:28:14 -0700

    final push

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_num = {'I': 1,'X': 10,  'L': 50, 'C': 100, 'D': 500,'M': 1000}
+    if roman_string is not str or roman_string is None:
+        return 0
+
+    decs = [roman_dec[x] for x in roman_string]
+
+    num = 0
+
+    for i in range(decs):
+        num  += decs[i]
+        if decs[i - 1] < decs[i] and i != 0:
+            num -= (decs[i - 1] + decs[i - 1])
+    return num
