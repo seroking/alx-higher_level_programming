745d5ac83d30399a8474822f1721f792555b7e93
seroking <baridmohamedamine004@gmail.com>
2023-06-14 16:30:30 -0700

    final push

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman_num = {'I': 1,'X': 10,  'L': 50, 'C': 100, 'D': 500,'M': 1000}
-    if roman_string is not str or roman_string is None:
+    if type(roman_string) is not str or roman_string is None:
         return 0
 
     decs = [roman_num[x] for x in roman_string]
