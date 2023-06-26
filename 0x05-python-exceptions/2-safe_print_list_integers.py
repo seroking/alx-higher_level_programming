0f16762aaf90273aca31b989f8d3321e5e55e7e3
seroking <baridmohamedamine004@gmail.com>
2023-06-26 07:23:16 -0700

    tasks

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for y in range(0, x):
+        try:
+            print("{:d}".format(my_list[y]), end="")
+            count +=1
+        except (TypeError, ValueError, IndexError):
+            continue
+
+    print()
+    return(count)
