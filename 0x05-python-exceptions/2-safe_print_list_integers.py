9c9d98dc41f6c188f422c06319c2fe5a3b16c1cc
seroking <baridmohamedamine004@gmail.com>
2023-06-26 07:24:47 -0700

    tasks

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,9 @@ def safe_print_list_integers(my_list=[], x=0):
     for y in range(0, x):
         try:
             print("{:d}".format(my_list[y]), end="")
-            count +=1
+            count += 1
         except (TypeError, ValueError):
             continue
 
     print()
-    return(count)
+    return (count)
