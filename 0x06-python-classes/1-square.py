85616c15d9b7b8277019ea02ee169d5fbd5d1038
seroking <baridmohamedamine004@gmail.com>
2023-06-27 09:20:29 -0700

    tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" class square """
+
+class Square:
+    """ square """
+    def __init__(self, size):
+        """initialise a square
+        Attributes:
+            size (int): size of square.
+        """
+        self.__size = size
