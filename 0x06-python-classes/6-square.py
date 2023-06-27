efa0cdd082ec280692a78dadb6a7e9aee0fc4c2c
seroking <baridmohamedamine004@gmail.com>
2023-06-27 16:38:52 -0700

    tired

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,9 +32,6 @@ class Square:
 
     def my_print(self):
         """Print the square with '#' ."""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
         if self.__size == 0:
             print("")
             return
