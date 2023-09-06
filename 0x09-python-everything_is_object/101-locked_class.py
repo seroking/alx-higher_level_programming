89d9875c4791501bdb9981573394d8bdeb40745c
seroking <baridmohamedamine004@gmail.com>
2023-09-06 15:23:44 -0700

    object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,6 +2,6 @@
 """ LockedClass
 """
 class LockedClass:
-            if name != "first_name" and hasattr(self, name):
-                pass
-        super().__setattr__(name, value)
+    if name != "first_name" and hasattr(self, name):
+        raise AttributeError("You can only set the 'first_name' attribute")
+    super().__setattr__(name, value)
