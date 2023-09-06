6cf13d619e94e3b500046998adc362f5027080be
seroking <baridmohamedamine004@gmail.com>
2023-09-06 15:24:10 -0700

    object

--- 0x09-python-everything_is_object/101-locked_class.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""" LockedClass
-"""
-class LockedClass:
-    if name != "first_name" and hasattr(self, name):
-        raise AttributeError("You can only set the 'first_name' attribute")
-    super().__setattr__(name, value)
