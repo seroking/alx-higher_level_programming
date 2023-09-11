70086f019a7e02cdb0c3480ae15182fc073c771a
seroking <baridmohamedamine004@gmail.com>
2023-09-11 14:14:01 -0700

    inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+module : 3-is_kind_of_class.py
+
+function :
+    is_kind_of_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    return true or false
+    """
+    return isinstance(obj, a_class)
