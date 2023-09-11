f6787b0d316c8d06d83a76b86f4c243e2b1f6aaf
seroking <baridmohamedamine004@gmail.com>
2023-09-11 15:02:33 -0700

    inheritance

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+module : 5-base_geometry.py
+
+class:
+    BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ empty class"""
+    def area(self):
+        """method that raise ana exeption """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validate the value"""
+        if type(value) not in [str, int]:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
