6cbf448e08a05cd81876efffd18124aa9ef7f4e9
seroking <baridmohamedamine004@gmail.com>
2023-09-11 14:42:37 -0700

    inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+module : 5-base_geometry.py
+
+class:
+    BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ empty class """
+    def area(self):
+        """method that raise ana exeption """
+        raise Exception("area() is not implemented")
