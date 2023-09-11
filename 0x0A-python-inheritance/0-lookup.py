6f9f2dfe0ba97689bde1b2bfd5db391fd880c5af
seroking <baridmohamedamine004@gmail.com>
2023-09-11 09:29:29 -0700

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" module : 0-lookup.py"""
+"""
+module : 0-lookup.py
+functions:
+    def lookup(obj)
+"""
 def lookup(obj):
     """
     return the list of attributes and methods of an objects
