fa2e7fe2ec75f5b329edd8e621ce1ad821838368
seroking <baridmohamedamine004@gmail.com>
2023-09-12 10:35:17 -0700

    input/output

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module : '3-to_json_string.py'
+Functions:
+    to_json_string
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """ return JSON representation """
+    return(json.dumps(my_obj))
