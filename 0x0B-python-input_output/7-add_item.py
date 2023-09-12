179fc7efc9aa46011e800355ea2c15b18f10e63b
seroking <baridmohamedamine004@gmail.com>
2023-09-12 15:36:25 -0700

    input/output

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" module : 7-add_item.py"""
+
+
+
+from sys import argv
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+filename = "add_items.json"
+
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+for i in range(0, len(argv)):
+    items.append(argv)
+
+save_to_json_file(items, filename)
