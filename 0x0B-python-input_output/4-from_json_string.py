939146c197371d944a8760b3584217400377cf09
seroking <baridmohamedamine004@gmail.com>
2023-09-12 14:11:30 -0700

    input/output

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,7 +5,8 @@ module : '4-from_json_string.py'
 
 
 def from_json_string(my_str):
-    """ returns an object (Python data structure) represented by a JSON string"""
+    """ returns an object (Python data structure) represented
+        by a JSON string"""
     import json
 
     return json.loads(my_str)
