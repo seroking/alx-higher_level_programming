f2237a23a865c82739e72606ef66331675f81c62
seroking <baridmohamedamine004@gmail.com>
2023-09-12 09:06:17 -0700

    input/output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module : 0-read_file.py
+functions:
+    read_file
+"""
+
+
+def read_file(filename=""):
+    """ read a file """
+    with open(filename, encoding="utf-8") as f:
+        read_file = f.read()
+    print(read_file)
