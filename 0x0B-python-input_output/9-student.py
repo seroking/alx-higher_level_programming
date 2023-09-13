01721349a02d48888ee63c1d6f587493f433dec7
seroking <baridmohamedamine004@gmail.com>
2023-09-12 17:20:27 -0700

    input/output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,7 +4,7 @@
 
 class Student:
     """ representation of the class student """
-    def __init__(self,first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
         """ Student representation"""
         self.first_name = first_name
         self.last_name = last_name
@@ -14,7 +14,8 @@ class Student:
         """ retrieves a json repr of dictionary of student"""
         my_dict = {}
         for item in dir(self):
-            if (not item.startswith("__")) and (not callable(getattr(self, item))):
+            if (not item.startswith("__")) /
+            and (not callable(getattr(self, item))):
                 my_dict[item] = getattr(self, item)
 
         return my_dict
