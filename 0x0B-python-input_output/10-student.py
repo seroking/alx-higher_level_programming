02885d1372296b069d424965473baad50b8c39c5
seroking <baridmohamedamine004@gmail.com>
2023-09-12 17:26:40 -0700

    input/output

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" module: 9-student.py"""
+
+
+class Student:
+    """ representation of the class student """
+    def __init__(self, first_name, last_name, age):
+        """ Student representation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a json repr of dictionary of student"""
+        my_dict = {}
+        for item in dir(self):
+            cond1 = not item.startswith("__")
+            cond2 = not callable(getattr(self, item))
+            if type(attrs) is list:
+                cond3 = item in attrs
+            if cond1 and cond2 and cond3:
+                my_dict[item] = getattr(self, item)
+
+        return my_dict
