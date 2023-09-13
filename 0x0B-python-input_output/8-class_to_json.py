296fef3a43ec882a310b75379bbf8929568547f9
seroking <baridmohamedamine004@gmail.com>
2023-09-12 17:19:01 -0700

    input/output

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,7 +6,8 @@ This is '8-class_to_json' module.
 
 
 def class_to_json(obj):
-    """returns the dictionary description for JSON serialization of an object"""
+    """returns the dictionary description for JSON
+       serialization of an object"""
     my_dict = {}
     for item in dir(obj):
         if (not item.startswith("__")) and (not callable(getattr(obj, item))):
---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" module: 9-student.py"""
+
+
+class Student:
+    """ representation of the class student """
+    def __init__(self,first_name, last_name, age):
+        """ Student representation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a json repr of dictionary of student"""
+        my_dict = {}
+        for item in dir(self):
+            if (not item.startswith("__")) and (not callable(getattr(self, item))):
+                my_dict[item] = getattr(self, item)
+
+        return my_dict
