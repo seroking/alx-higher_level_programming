bee72bdc3ae60f9bbe8fb4b0c0763c4812db31c8
seroking <baridmohamedamine004@gmail.com>
2023-10-17 12:06:19 -0700

    Mysql

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,15 @@
+-- print the full description of a table
+
+SELECT
+    COLUMN_NAME,
+    DATA_TYPE,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = DATABASE() AND
+    TABLE_NAME = 'first_table';
