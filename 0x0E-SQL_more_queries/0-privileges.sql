ac30810be4bed1a6c2498c707cc71d94b69df65e
seroking <baridmohamedamine004@gmail.com>
2023-10-18 07:42:01 -0700

    more sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- lists all privileges of a MySQL users
 
-SHOW GRANTS FOR 'user_0d_1'@'loacalhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'loacalhost', 'user_0d_2'@'localhost';
