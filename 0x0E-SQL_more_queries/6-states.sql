d613846ba1540525bf02bf1e3458e3677ac7b4fc
seroking <baridmohamedamine004@gmail.com>
2023-10-18 15:28:02 -0700

    Mysql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- more database and table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
