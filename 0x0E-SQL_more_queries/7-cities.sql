ea313b990742f33626e5a9e8ebaf5577b3676bef
seroking <baridmohamedamine004@gmail.com>
2023-10-18 16:55:19 -0700

    Mysql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- more database and tables *_*
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id),
+    name VARCHAR(256)
+);
