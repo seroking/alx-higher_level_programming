6783fa87a56bcc1b975166deabafaa3a8d69d381
seroking <baridmohamedamine004@gmail.com>
2023-11-13 19:24:31 +0100

    js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,19 +1,5 @@
 #!/usr/bin/node
 
-const args = process.argv;
-let arg_l = args.length - 2;
+const args = process.argv.length - 2;
 
-if (arg_l === 0)
-{
-  console.log('No argument');
-}
-else if (arg_l === 1)
-{
-  console.log('Argument found');
-}
-else
-{
-  console.log('Arguments found');
-}
-
-console.log(arg_l)
+console.log(count === 0 'No argument' : count === 1 'Argument found' : 'Arguments found')
