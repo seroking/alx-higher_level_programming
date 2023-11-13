c21c9a36eb01029c418b67a06338a3b145bea32f
seroking <baridmohamedamine004@gmail.com>
2023-11-13 20:27:56 +0100

    js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
 let count = 0;
-while (args[count] !== undefined)
-{
+while (args[count] !== undefined) {
   count++;
 }
 console.log(count === 2 ? 'No argument' : args[2]);
