b9024eca56e0ee14e6538b018285bb4e3e8d034d
seroking <baridmohamedamine004@gmail.com>
2023-11-14 17:55:38 +0100

    js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
+    }
+  }
+
+  print () {
+    for (let i = 1; i <= this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+};
