0d0173f5396582975c74123cf6b9bbb97d8dcbbd
seroking <baridmohamedamine004@gmail.com>
2023-11-14 16:59:19 +0100

    js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,6 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 || h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 };
