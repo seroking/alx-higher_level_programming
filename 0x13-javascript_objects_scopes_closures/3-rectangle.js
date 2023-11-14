fe72dda484d6afdf569fd28fa2e96c39837fce44
seroking <baridmohamedamine004@gmail.com>
2023-11-14 17:21:15 +0100

    js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,13 @@
+module.exports = class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
+    }
+  }
+
+  print() {
+    for (let i = 1; i < this.height; i++) {
+      console.log("X".repeat(this.width));
+    }
+  }
+};
