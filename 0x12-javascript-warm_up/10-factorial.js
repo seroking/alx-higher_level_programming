3c896db91be001b9101bbca03bbb11871e504a63
seroking <baridmohamedamine004@gmail.com>
2024-01-08 23:07:19 +0100

    .

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function factorial (n) {
+    let fac = 1;
+    for (let i = 1; i < n + 1; i++) {
+      fac = fac * i;
+    }
+    return (fac);
+  }
+  console.log(factorial(parseInt(process.argv[2])));
