7bce530924d13383785c85f89e93159c61008f63
seroking <baridmohamedamine004@gmail.com>
2024-01-10 22:36:45 +0100

    js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 
 const arr1 = require('./100-data').list;
-
-const arr2 = arr1.map((x, idx) => x * idx);
-
 console.log(arr1);
-console.log(arr2);
+console.log(arr1.map((x, idx) => x * idx));
