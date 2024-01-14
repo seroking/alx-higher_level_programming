e2a2af55ec132a26451c674812938d79e16e64e0
seroking <baridmohamedamine004@gmail.com>
2024-01-14 08:07:00 -0800

    orm

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+list all state ordered by id
+
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3]))
+    conn = engine.connect()
+    result = conn.execute('SELECT * FROM states ORDER BY id')
+
+    for row in result.fetchall():
+        print("{}: {}".format(row['id'], row['name']))
