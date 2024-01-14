91329ad0b77fc9cd1418a5bab27c323f1e5182f7
seroking <baridmohamedamine004@gmail.com>
2024-01-14 13:04:15 -0800

    ORM

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,7 +16,7 @@ if __name__=="__main__":
 
     states = session.query(State).order_by(State.id).first()
 
-    if (states):
-        print("{}: {}".format(states.id, states.name))
-    else:
+    if not states:
         print("Nothing")
+    else:
+        print("{}: {}".format(states.id, states.name))
