69d24cdf5cd0ed5616fe81f20bb2ac675a608e18
seroking <baridmohamedamine004@gmail.com>
2024-01-14 13:05:45 -0800

    ORM

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -7,9 +7,9 @@ from sqlalchemy import create_engine
 from sys import argv
 from model_state import Base, State
 
-if __name__=="__main__":
+if __name__ == "__main__":
     engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(
-            argv[1], argv[2],argv[3], pool_pre_ping=True))
+            argv[1], argv[2], argv[3], pool_pre_ping=True))
 
     Session = sessionmaker(bind=engine)
     session = Session()
