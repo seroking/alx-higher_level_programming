ff6fefb027f00ef408c6a46d3f4a97deb438c8dc
seroking <baridmohamedamine004@gmail.com>
2024-01-14 08:14:19 -0800

    orm

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,10 +9,10 @@ from sqlalchemy.orm import Session
 from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1],
         argv[2],
-        argv[3]))
+        argv[3], pool_pre_ping=True))
 
     with engine.connect() as conn:
         result = conn.execute('SELECT * FROM states ORDER BY id')
