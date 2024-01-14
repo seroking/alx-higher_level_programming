eb093bd71751e063234721e97486731361843da2
seroking <baridmohamedamine004@gmail.com>
2024-01-14 13:02:44 -0800

    ORM

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,7 +9,7 @@ from model_state import Base, State
 
 if __name__=="__main__":
     engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(
-            argv[1], argv[2],argv[3]))
+            argv[1], argv[2],argv[3], pool_pre_ping=True))
 
     Session = sessionmaker(bind=engine)
     session = Session()
