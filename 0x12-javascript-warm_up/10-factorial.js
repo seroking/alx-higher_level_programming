ca8f99c6ed6f77b8eb5092c087465e40c7623baa
seroking <123641553+seroking@users.noreply.github.com>
2024-03-07 14:19:13 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/10-factorial.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-function factorial (n) {
-  let fac = 1;
-  for (let i = 1; i < n + 1; i++) {
-    fac = fac * i;
-  }
-  return (fac);
-}
-console.log(factorial(parseInt(process.argv[2])));
